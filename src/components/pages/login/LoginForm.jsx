c8f1bf35f279ded0e2e316fd3cc6035a136436b9
Kai Lam <kai_lam@hotmail.fr>
2025-09-17 21:28:25 +0200

    ♻️ F01: LOGIN > Create LoginForm

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react"
+import React, { useState } from 'react'
 
-export default function LoginPage() {
+export default function LoginForm() {
     //state
     const [inputValue, setInputValue] = useState("")
     
@@ -14,16 +14,14 @@ export default function LoginPage() {
     const handleChange = (event) => {
         setInputValue(event.target.value)
     }
-    //affichage
+
   return (
-    <div>
-        <h1>Bienvenue chez nous</h1>
-        <br />
-        <h2>Connectez-vous</h2>
         <form action="submit" onSubmit={handleSubmit}>
+            <h1>Bienvenue chez nous</h1>
+            <br />
+            <h2>Connectez-vous</h2>
             <input value={inputValue} onChange={handleChange} type="text" placeholder='Entrez votre prénom' required/>
             <button>Accédez à votre espace</button>        
-        </form>
-    </div>
-  )
+        </form> 
+    )
 }
