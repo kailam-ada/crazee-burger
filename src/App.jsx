a1c87ef927faf32c070f635640cb383ebb3b8778
Kai Lam <kai_lam@hotmail.fr>
2025-09-19 13:20:34 +0200

    ✨ F02: APP > install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import LoginPage from './components/pages/login/LoginPage'
+import { Route, Routes } from 'react-router-dom'
+import OrderPage from './components/pages/order/OrderPage'
+import ErrorPage from './components/pages/error/ErrorPage'
 
 function App() {
 
   return (
-    <LoginPage /> 
+    <Routes>
+      <Route path='/' element={<LoginPage />} />
+      <Route path='/order' element={<OrderPage />} />
+      <Route path='/*' element={<ErrorPage />} />
+    </Routes>
   )
 }
 
---
+++ src/components/pages/order/OrderPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function OrderPage() {
+    
+  return (
+    <div>OrderPage</div>
+  )
+}
