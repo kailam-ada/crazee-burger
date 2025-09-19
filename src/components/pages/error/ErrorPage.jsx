0727b8447dbe322994b0f85dd85d2d8b18e27116
Kai Lam <kai_lam@hotmail.fr>
2025-09-19 13:36:37 +0200

    ✨ F02: LOGIN > add redirection from/to ErrorPAge

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div>
+        <Link to='/'>
+            <button>Retourner vers la page d'accueil</button>
+        </Link>
+    </div>
   )
 }
